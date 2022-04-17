10539920d99faebf4742f9273601b1d5b5d18b0a
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-04-17 09:31:21 +0600

    firebase auth google ,email password and git hub is set

--- src/sign-out-up-auth/Form/Form.js
+++ src/sign-out-up-auth/Form/Form.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css'
+import { useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import img1 from '../../img/auth-icon/1.png'
 import img2 from '../../img/auth-icon/2.png'
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+
+
 
 const Form = ({ data }) => {
+    // email and password usestate
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+    const [confirmpassword, setcnfirmpassword] = useState('')
+    const [passworderror, setpassworderror] = useState('')
+
+    const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true })
+
+    const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+
+    const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+    const [signInWithGithub] = useSignInWithGithub(auth);
+    const heandelSignInSignUp = (e) => {
+        e.preventDefault();
+        setemail(e.target.email.value);
+        setpassword(e.target.password.value);
+        setcnfirmpassword(e.target.CofrimPassword.value);
+
+    };
+    const hendelcreateUser = () => {
+        if (password === confirmpassword) {
+            createUserWithEmailAndPassword(email, password)
+            setpassworderror('')
+        }
+        else {
+            setpassworderror('Your password and confirm password are not maching')
+        }
+    }
+    const hendelcreateUserlogIn = () => {
+        signInWithEmailAndPassword(email, password)
+    }
     return (
         <div className='form-main-div'>
             {/* form title  */}
@@ -12,23 +48,26 @@ const Form = ({ data }) => {
                 data ? <h1 className='form-title'>Please Sign Up</h1> : <h1 className='form-title'>Please Log In</h1>
             }
             {/* all input filds  */}
-            <form >
+            <form onSubmit={heandelSignInSignUp}>
                 <div className='all-inputfild-div '>
                     <div>
                         {
                             data ? <div>
-                                <input className='input-filds' type="name" name="name" placeholder='Enter Name' />
+                                <input className='input-filds' type="name" name="name" placeholder='Enter Name' required />
                             </div> : ""
                         }
                         <div>
-                            <input className='input-filds' type="email" name="email" placeholder='Enter Email Id' />
+                            <input className='input-filds' type="email" name="email" placeholder='Enter Email Id' required />
                         </div>
 
                         <div>
-                            <input className='input-filds' type="password" name="password" placeholder='Enter Password' />
+                            <input className='input-filds' type="password" name="password" placeholder='Enter Password' required />
                         </div>
                         {
-                            data ? <input className='input-filds' type="password" name="CofrimPassword" placeholder='Enter Cofrim Password' /> : ''
+                            data ? <div>
+                                <input className='input-filds' type="password" name="CofrimPassword" placeholder='Enter Cofrim Password' required />
+                                <p className='w-75 ms-5 text-danger'>{passworderror}</p>
+                            </div> : ''
                         }
 
                         {
@@ -37,9 +76,9 @@ const Form = ({ data }) => {
                         }
                         {
                             data ? <div>
-                                <button className='submit-button'>Sign Up</button>
+                                <button className='submit-button ' onClick={hendelcreateUser} >Sign Up</button>
                             </div> : <div>
-                                <button className='submit-button'>Log In</button>
+                                <button className='submit-button' onClick={hendelcreateUserlogIn}>Log In</button>
                             </div>
                         }
 
@@ -50,8 +89,8 @@ const Form = ({ data }) => {
                             <hr />
                         </div>
                         <div className='main-icon-div'>
-                            <img className='icon-img' src={img2} alt="/" />
-                            <img className='icon-img' src={img1} alt="/" />
+                            <img className='icon-img' onClick={() => signInWithGoogle()} src={img2} alt="/" />
+                            <img className='icon-img' onClick={() => signInWithGithub()} src={img1} alt="/" />
                         </div>
                     </div>
                 </div>
